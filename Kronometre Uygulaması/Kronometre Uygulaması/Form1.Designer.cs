85217415af90d7759a766ab779944791958bca2a
Efe Yanar <efe1yanar@gmail.com>
2024-04-04 00:06:30 +0300

    Add files via upload

---
+++ Kronometre Uygulaması/Kronometre Uygulaması/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Kronometre_Uygulaması
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.basla = new System.Windows.Forms.Button();
+            this.durdur = new System.Windows.Forms.Button();
+            this.sifirla = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(81, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DAKİKA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(81, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "SANİYE";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(81, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 25);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "SALİSE";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(228, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 25);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(228, 131);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 25);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(228, 195);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 25);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "label6";
+            // 
+            // basla
+            // 
+            this.basla.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.basla.Location = new System.Drawing.Point(424, 50);
+            this.basla.Name = "basla";
+            this.basla.Size = new System.Drawing.Size(183, 58);
+            this.basla.TabIndex = 4;
+            this.basla.Text = "BAŞLA";
+            this.basla.UseVisualStyleBackColor = true;
+            this.basla.Click += new System.EventHandler(this.basla_Click);
+            // 
+            // durdur
+            // 
+            this.durdur.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.durdur.Location = new System.Drawing.Point(424, 114);
+            this.durdur.Name = "durdur";
+            this.durdur.Size = new System.Drawing.Size(183, 58);
+            this.durdur.TabIndex = 4;
+            this.durdur.Text = "DURDUR";
+            this.durdur.UseVisualStyleBackColor = true;
+            this.durdur.Click += new System.EventHandler(this.durdur_Click);
+            // 
+            // sifirla
+            // 
+            this.sifirla.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sifirla.Location = new System.Drawing.Point(424, 178);
+            this.sifirla.Name = "sifirla";
+            this.sifirla.Size = new System.Drawing.Size(183, 58);
+            this.sifirla.TabIndex = 4;
+            this.sifirla.Text = "SIFIRLA";
+            this.sifirla.UseVisualStyleBackColor = true;
+            this.sifirla.Click += new System.EventHandler(this.sifirla_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 328);
+            this.Controls.Add(this.sifirla);
+            this.Controls.Add(this.durdur);
+            this.Controls.Add(this.basla);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "KRONOMETRE";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button basla;
+        private System.Windows.Forms.Button durdur;
+        private System.Windows.Forms.Button sifirla;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Kronometre Uygulaması/Kronometre Uygulaması/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kronometre_Uygulaması
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int dakika = 0, saniye = 0, salise = 1;
+
+        private void durdur_Click(object sender, EventArgs e)
+        {
+            durdur.Enabled = false;
+            sifirla.Enabled = true;
+            timer1.Stop();
+            basla.Enabled = true;
+            basla.Text = "DEVAM ET";
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (salise == 60)
+            {
+                saniye++;
+                salise = 0;
+                if (saniye == 60)
+                {
+                    dakika++;
+                    saniye = 0;
+                }
+            }
+            label4.Text = Convert.ToString(dakika);
+            label5.Text = Convert.ToString(saniye);
+            label6.Text = Convert.ToString(salise);
+            salise++;
+        }
+
+        private void sifirla_Click(object sender, EventArgs e)
+        {
+            dakika = 0;
+            saniye = 0;
+            salise = 0;
+            basla.Enabled = true;
+            durdur.Enabled = false;
+            sifirla.Enabled = false;
+            basla.Text = "BAŞLA";
+            label4.Text = "0";
+            label5.Text = "0";
+            label6.Text = "0";
+        }
+
+        private void basla_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            basla.Enabled = false;
+            durdur.Enabled = true;
+            sifirla.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 10;
+            label4.Text = "0";
+            label5.Text = "0";
+            label6.Text = "0";
+            durdur.Enabled = false;
+            sifirla.Enabled = false;
+        }
+    }
+}
